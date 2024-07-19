f1b2ef77430b20bad8dd21b9442dde17dbd88ffe
김보경 <qhrud7127@naver.com>
2024-07-19 16:04:10 +0900

    dashboard 현황 수정

--- src/sections/analysics/dashboard-state-view.tsx
+++ src/sections/analysics/dashboard-state-view.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import Avatar from '@mui/material/Avatar';
 import Divider from '@mui/material/Divider';
-import Grid from '@mui/material/Unstable_Grid2';
 import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
 
@@ -15,28 +14,24 @@ export const DashboardStateView = ({ totalCount }: Props) => {
   return (
     <Card className="dashboard-state">
       <CardHeader title="API 현황" />
-      <Grid container spacing={1} className="item" sx={{ margin: '65px 58px' }}>
-        <Grid xs={6} md={6} lg={6}>
-          <Typography variant="h6" sx={{ textAlign: 'center' }}>
+      <Box display="grid" gap={2} gridTemplateColumns="repeat(2, 1fr)" sx={{ p: 3, my: 6 }}>
+        <Box className="item">
+          <Typography variant="h6" sx={{ mt: 1 }}>
             연계상태
           </Typography>
-        </Grid>
-        <Grid xs={6} md={6} lg={6}>
-          <Typography variant="h6" sx={{ textAlign: 'center' }}>
+          <Typography variant="h2" sx={{ color: 'info.main' }}>
+            정상
+          </Typography>
+        </Box>
+        <Box className="item">
+          <Typography variant="h6" sx={{ mt: 1 }}>
             오류건수
           </Typography>
-        </Grid>
-        <Grid xs={6} md={6} lg={6}>
-          <Avatar sx={{ width: 100, height: 100, fontSize: '30px' }} color="info">
-            정상
-          </Avatar>
-        </Grid>
-        <Grid xs={6} md={6} lg={6}>
-          <Avatar sx={{ width: 100, height: 100, fontSize: '30px' }} color="error">
+          <Typography variant="h2" sx={{ color: 'error.main' }}>
             0
-          </Avatar>
-        </Grid>
-      </Grid>
+          </Typography>
+        </Box>
+      </Box>
       <Divider />
       <Typography variant="h6" sx={{ textAlign: 'center', margin: '10px 0' }}>
         {`총 누적건수 : ${count} 건`}
